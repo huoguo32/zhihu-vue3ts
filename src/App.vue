462e6a77de397b8ef7e0f9bb66c5be087d11014a
huoguo32 <326891901@qq.com>
2022-06-16 16:23:36 +0800

    ts

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<div class="container"></div>
+<ColumnList :list="list"></ColumnList>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  }
+]
+export default defineComponent({
+  name: 'App',
+  components: {
+    ColumnList
+  },
+  setup (props, content) {
+    // const columnList = reactive(testData)
+    return {
+      list: testData
+    }
+  }
+})
+</script>
 
 <style lang="less">
 #app {
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul>
+    <li v-for="column in list " :key="column.id">
+    <img :src="column.avatar" :alt="column.title">
+    <h5>{{column.title}}</h5>
+    <p>{{column.description}}</p>
+    <a href="#">进入专栏</a>
+    </li>
+  </ul>
+</template>
+<script lang="ts">
+import {
+  reactive,
+  toRefs,
+  onBeforeMount,
+  onMounted,
+  defineComponent,
+  ref,
+  PropType
+} from 'vue'
+export interface ColumnProps {
+  id: number
+  title: string
+  description: string
+  avatar: string
+}
+export default defineComponent({
+  name: 'App',
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  },
+  setup (props, content) {
+    // props.list[0].id
+  }
+})
+</script>
+<style lang="scss" scoped></style>
